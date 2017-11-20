d81c504ccadca408afc038b31d860f8ee8be76a3
Dershen Anton <a.dershen@croc.ru>
2017-11-20 18:09:01 +0300

    Инциализация проекта

---
+++ Project/Project/Controllers/PageAPIController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Project.Controllers
+{
+    public class PageAPIController : BaseAPIController
+    {
+        public HttpResponseMessage Get()
+        {
+            return ToJson(new string[] { "value1", "value2" });
+        }
+        public HttpResponseMessage Get(int id)
+        {
+            return ToJson("value");
+        }
+        public void Post([FromBody]string value)
+        {
+        }
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Project/Project/angular_app/components/app.component/app.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app',
+    templateUrl: './angular_app/components/app.component/app.component.html'
+})
+export class AppComponent {
+    name = '';
+}
